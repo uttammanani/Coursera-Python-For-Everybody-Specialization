c4e76145f1b4fa72a51040aa7bdd8b3ce6de1fa3
Uttamkumar Manani <uttammanani@gmail.com>
2021-07-03 20:52:11 +0530

    Added Assignment 7.2 and mbox-short.txt Files

---
+++ Python-Data-Structures/Week-3/Assignment7-2.py
@@ -0,0 +1,18 @@
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter File Name: ")
+fh=open(fname)
+
+cnt = 0
+floatSum = 0
+
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    else:
+        cnt += 1
+        findNum = line.find('0')
+        x = line[findNum:]
+        floatSum = floatSum + float(x)
+
+avg = float(floatSum/cnt)
+print("Average spam confidence:",avg)
