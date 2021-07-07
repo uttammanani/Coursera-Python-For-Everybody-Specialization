fff3832e7fabd76489626c241e35a144d4615600
Uttamkumar Manani <uttammanani@gmail.com>
2021-07-08 02:08:03 +0530

    Added Week-4 Assignment 8.5 and mbox-short.txt

---
+++ Python-Data-Structures/Week-4/Assignment8-5.py
@@ -0,0 +1,13 @@
+file = input('Please enter file name:')
+handle = open(file)
+count = 0
+
+for line in handle:
+	line = line.rstrip()
+	if not line.startswith('From '):
+		continue
+	line = line.split()
+	print(line[1])
+	count = count + 1
+
+print('There were', count, 'lines in the file with From as the first word')
